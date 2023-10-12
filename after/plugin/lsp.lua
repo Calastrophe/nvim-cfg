f71baf1b5e2a65c15abb1f2b75fb4fbadaa97b98
neo <neo@pop-os.localdomain>
2023-10-11 23:09:48 -0500

    redo templating

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,6 +2,7 @@ local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
+  lsp_zero.buffer_autoformat()
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -20,16 +21,27 @@ require('mason-lspconfig').setup({
   ensure_installed = {'rust_analyzer'},
   handlers = {
     lsp_zero.default_setup,
-  },
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  }
 })
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
 cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+  },
+  formatting = lsp_zero.cmp_format(),
   mapping = cmp.mapping.preset.insert({
-	  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	  ["<C-Space>"] = cmp.mapping.complete(),
-  })
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
 })
